b085cd600cc1ba7d59cfdd500ded28c489163631
dmitry <dik100695@gmail.com>
2022-12-30 21:07:34 +0300

    Функции

--- JS/app.js
+++ JS/app.js
@@ -1,93 +1,69 @@
+// ФУНКЦИИ
 
-
-const role = 'medik';
-
-if (role === 'maneg') { 
-	console.log('yyyy');
-} else if (role === 'seo') {
-	console.log('seoseo');
-} else if(role === 'medik') {
-	console.log('medikmedik');
-} else {
-	console.log('nonono');
+function numRes(num1, num2) {
+	const res = num1 * num2
+	console.log(`${num1} ${num2} получится ${res}`);
+	return res;
 }
+console.log(numRes(3, 4));
 
-const role1 = 'maneg';
-/*
-switch (role1) { 
-	case 'maneg1':
-		console.log('1yyyy');
-		break;
-	case 'seo1':
-		console.log('1seoseo');
-		break;
-	case 'medik1':
-		console.log('1medikmedik');
-		break;
-	default:
-		console.log('nonono');
-}
+numRes(5, 6);
+
+//Анонимная
+
+let anonimnayFun = function (num1, num2) {
+	const res = num1 * num2
+	console.log(`${num1} *${num2} получится ${res}`);
+	return res;
+};
+
+console.log(anonimnayFun(17,23));
+anonimnayFun(40, 5);
+
+// Стрелочные
+
+let strelochnayFun = (num1, num2) => num1 * num2;
+
+let a1 = 60,
+	a2 = anonimnayFun(3,3);
+
+
+console.log(`${a1} умножить ${a2} получится ${strelochnayFun(a1, a2)}`);
+
+/*	
+Задание для упражнения:
+Пользователь:
+Возраст
+Availability of work
+Деньги
+Нужно проверить может ли он купить новый MacBook за 2000$? Он может брать не только свои деньги, но и взять кредит. Ему дадут 500$, только если ему больше 24-х лет и он имеет работу, 100$ если ему просто больше 24-х лет и 0 в ином случае. Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false.
 */
+// const costOfGoods = 2000;
+// const userAge = 25;
+// const userWork = false;
+// const userMoney = 1000;
 
-const role2 = 'medik';
 
-switch (role2) { 
-	case 'maneg1':
-	case 'seo1':
-		console.log('yyyyyyyyyyyyyy');
-		break;
-	case 'medik1':
-		console.log('1medikmedik');
-		break;
-	default:
-		console.log('nonono');
+
+function rez (costOfGoods, userAge, userMoney, userWork = false) {
+	if (userWork && userAge > 24) {
+		userMoneyCredit = 500;
+	} else if (userAge > 24) {
+		userMoneyCredit = 100;
+	} else {
+		userMoneyCredit = 0;
+	}
+	let balans = userMoneyCredit + userMoney;
+	
+	let answer;
+	if (balans >= costOfGoods) {
+		answer = true;
+	} else {
+		answer = false;
+	}
+	return answer;
 }
 
 
-// Задача. В переменной min лежит число от 0 до 59. Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+console.log(rez(2000, 25, 1600, ) ? 'может купить':'не может купить');
 
-// Решение:
-const min = 59;
-
-if (min < 15 && min >= 0) {
-	console.log('в первую');
-} else if (min >=15 && min <30) {
-	console.log('вторую');
-} else if (min >= 30 && min < 45) {
-	console.log('третью');
-} else if (min >= 45 && min < 60) {
-	console.log('четвертую');
-} else {
-
-}
-
-// Переменная lang может принимать 2 значения: 'ru' 'en'. Если она имеет значение 'ru', то в переменную arr запишем массив дней недели на русском языке, а если имеет значение 'en' – то на английском. Решите задачу через 2 if, через switch-case и через многомерный массив без ифов и switch.
-
-let lang = 'en';
-let arr,
-	arrErorr = 'Erorr';
-const arrEn  = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
-	arrRu  = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-let rezTest = lang === 'ru' || lang === 'en';
-
-console.log(rezTest);
-lang === 'ru' ?  arr = arrRu : arr = arrEn;
-console.log(rezTest === true ? arr : arrErorr);
-
-
-// Пользователь хочет приобрести игру в магазине. Он может это сделать только если:
-// Eго баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
-// Он не забанен (isBanned)
-// Игра не куплена (isExist)
-// Игра в продаже (isSelling)
-// Напишите условие для покупки и выведите в консоль результат.
-
-
-let balance = 1001,
-	bonusBalance = 10,
-	isBanned = true,
-	isExist = false,
-	isSelling = true;
-
-let canBuy = (balance > 1000 || bonusBalance > 100) && !isBanned && !isExist && isSelling;
-console.log(`Пользователь приоб ретёт игру? ${canBuy ? 'Да' : 'Нет'}`);

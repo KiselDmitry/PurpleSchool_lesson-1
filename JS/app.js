5ce8add3875c0d8086c7433a91bb52712395dad7
dmitry <dik100695@gmail.com>
2023-01-06 14:07:39 +0300

    Метод reverse() concat()

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,12 @@
 const roles = ['admin', 'user', 'menager', 'superuser'];
 
-//Метод slice(2) отрезает кусок массива начиная от указонного элемента , не изменяя исходный массив
-const res = roles.slice(2);
+//Метод reverse()  изменяет исходный массив в обратную сторону
 console.log(roles);
+roles.reverse();
+console.log(roles);
+
+const newRoles = ['sisadmin', 'developer'];
+
+//Метод concat()  конкатинирует
+const res = roles.concat(newRoles);
 console.log(res);
-
-//Метод slice(2, 3) отрезает кусок массива начиная от указонного элемента и до указанного во втором аргументе , не изменяя исходный массив
-const res2 = roles.slice(2,3);
-console.log(roles);
-console.log(res2);
-
-//Метод slice(-1) вернёт МАССИВ из последнего элемента, а -2 из двух последних
-const res3 = roles.slice(-2);
-console.log(roles);
-console.log(res3);
-
-//Метод splice(2) отрезает кусок массива начиная от указонного элемента , и зменяет исходный массив остовляя в нём то что не отрезалось
-const res4 = roles.splice(2);
-console.log(roles);
-console.log(res4);
-
-//Метод splice(2, 2) второй аргумент указывает сколько элементов обрезать начиная от элемента указанного в первом аргументе
-const res5 = roles.splice(1, 2);
-console.log(roles);
-console.log(res5);

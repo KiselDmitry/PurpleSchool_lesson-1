61a92878d460d40d14b9d512d3dea0c9215e22b8
dmitry <dik100695@gmail.com>
2023-01-07 16:38:09 +0300

    Цыклы

--- JS/app.js
+++ JS/app.js
@@ -9,7 +9,7 @@ for (let i = arr.length - 1; i >= 0; i--) {
 	arrRes.push(arr[i]);
 
 }
-const res = arrRes.join(" ")
+const res = arrRes.join(" ");
 console.log(res);
 
 // Вариант 2
@@ -20,3 +20,42 @@ for (let i = 0; i < arr.length; i++) {
 }
 const res2 = arrRes2.join(" ")
 console.log(res2);
+
+// Вариант 3 цикл wile
+
+const arr3 = ['!', ' JS', 'люблю', 'Я'];
+const arrRes3 = [];
+let i = arr3.length - 1;
+
+while (i>=0) {
+	arrRes3.push(arr3[i]);
+	i--;
+}
+console.log(arrRes3.join(" "));
+
+// ====цикл do wile ======
+let j = 0;
+do {
+	console.log(j);
+	j++;
+} while (j < 0);
+
+// ====цикл for of перебор по элементам======
+
+const array1 = [1, -3, 5, 7, -9, -11];
+let resEl = 0;
+for (let element of array1) {
+	console.log(element);
+	resEl = resEl + element
+}
+console.log('===');
+console.log(resEl);
+console.log('===');
+
+// ====цикл for in перебор по индексу======
+
+const array2 = ['ф', 'ы', 'в', 'а', 'п', 'р'];
+for (let index in array2) {
+	console.log(index); // выводит индекс
+	console.log(array2[index]); //выводит элемент
+}

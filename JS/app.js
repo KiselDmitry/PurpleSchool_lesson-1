19c7e526421a580e464f84ff9c004e0bcf4be80a
dmitry <dik100695@gmail.com>
2023-01-07 10:12:21 +0300

    Деструктуризация Rest оператор

--- JS/app.js
+++ JS/app.js
@@ -1,75 +1,36 @@
-/*
-Дан список задач
-const tasks = ['Задача 1'];
+const userDate = ['Anton', 18, 'Mascow'];
 
-Сделать функции:
-Добавление задачи в конец
-Удаление задачи по названию
-Перенос задачи в начало списка по названию
-! Всегда меняем исходный массив
-push()
-includes('el')
-indexof()
-splice(2, 2)
-splice()
-unshift()
-*/
-
-const tasks = ['Задача 1'];
-
-//Добавление задачи в конец
-function addEnd(task) {
-	tasks.push(task);
-}
-addEnd('Задача2');
-addEnd('Задача3');
-addEnd('Задача4');
-console.log(tasks);
-//==========================
-
-//Удаление задачи по названию=====
-function deletTask(task) {
-	 
-	
-	const el = tasks.indexOf(task);
-	if (el === -1) {
-		return;
-	} 
-	const taskStart = tasks.splice(el, 1);
-	
+function getDate() {
+	return ['Anton', 18, 'Mascow'];
 }
 
-const start = deletTask('Задача2');
-console.log(tasks);
-//==========================
+const userName = getDate()[0];
+const age = getDate()[1];
+const city = getDate()[2];
 
-//Перенос задачи в начало списка по названию===
-function addBeginningList(task) {
-	const el = tasks.indexOf(task);
-	if (el === -1) {
-		return;
-	} 
-	//одно решение
-	const taskStart = tasks.splice(el, 1)[0];
-	tasks.unshift(taskStart);
-	//другое решение
-	// const oldTask = tasks[el];
-	// tasks.splice(el, 1)
-	// tasks.unshift(oldTask);
-} 
-addBeginningList('Задача4');
-console.log(tasks);
-console.log(tasks.includes('Задача6'));
-//==========================
+console.log(userName, age, city); //Anton 18 Mascow
 
+console.log(userDate); //['Anton', 18, 'Mascow']
 
+console.log(getDate()); //['Anton', 18, 'Mascow']
 
+//Деструктуризация=================
 
+//const userDate2 = ['Ivan', 22, 'Anapa'];
+function getDate2() {
+	return ['Ivan', 22, 'Anapa'];
+}
 
+const [userImy, userAge, userCity] = getDate2();
+//const [userImy, userAge, userCity] = userDate2;
 
+console.log(userImy, userAge, userCity); //Ivan 22 Anapa
+//console.log(userDate2); //['Ivan', 22, 'Anapa']
 
+//Rest оператор
 
+const data = [1, 2, 3, 4, 5, 6, 7];
 
+const [one, two, three, four, ...others] = data;
 
-
-
+console.log(one, two, three, others); //1 2 3   [5, 6, 7]

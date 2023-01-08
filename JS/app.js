e9bd5f67a1d78a1d5a2e639fefff7636bcc85713
dmitry <dik100695@gmail.com>
2023-01-08 10:56:46 +0300

    Возврат функции

--- JS/app.js
+++ JS/app.js
@@ -11,6 +11,7 @@ function power(a,b) {
 }
 
 // Фукция вышего порядка
+//функция передаётся в функцию
 function calculate(a,b,fn) {
 	console.log(fn.name);
 	const res = fn(a, b);
@@ -23,3 +24,21 @@ res = calculate(2, 3, subtract);
 console.log(res);
 res = calculate(2, 3, power);
 console.log(res);
+
+//функция возвращает функцию
+
+function power2(pow) {
+	return function (num) {
+		return num ** pow;
+	}
+}
+
+const powerOfTwo = power2(2);
+console.log(powerOfTwo(5));
+console.log(powerOfTwo(10));
+
+
+const powerOfThree = power2(3);
+console.log(powerOfThree(5));
+
+console.log(power2(5)(4));

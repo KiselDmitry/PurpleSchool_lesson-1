86262418256c87d245dc740cc9d43dff0b076c06
dmitry <dik100695@gmail.com>
2023-01-08 08:42:08 +0300

    Callback

--- JS/app.js
+++ JS/app.js
@@ -1,50 +1,25 @@
-
-const operations = [1000, -700, 300, -500, 10000];
-const startingBalans = 100;
-
-//======
-function average(arr, initailBalans) {
-	let sum = initailBalans;
-	for (const iterator of arr) {
-		sum += iterator;
-	}	
-	return sum;
+function add(a,b) {
+	return a + b;
 }
-console.log(average(operations, startingBalans));
 
-//======
-function balanceCheck(arr, initailBalans) {
-	let sum = initailBalans;
-	let isOk = true;
-	for (const iterator of arr) {
-		sum += iterator;
-		if (sum < 0) {
-			isOk = false;
-			break;
-		}
-		
-	}
-	return isOk;
-} 
-console.log(balanceCheck(operations, startingBalans));
-
-//======
-
-function checkPositivNgativ(arr) {
-	let positiveCount = 0;
-	let positiveSum = 0;
-	let negativeCount = 0;
-	let negativeSum = 0;
-	for (const iterator of arr) {
-		if (iterator > 0) {
-			positiveCount++;
-			positiveSum += iterator;
-		}
-		if(iterator < 0) {
-			negativeCount++;
-			negativeSum += iterator;
-		}
-	}
-	return [positiveSum / positiveCount, negativeSum / negativeCount];
+function subtract(a,b) {
+	return a - b;
 }
-console.log(checkPositivNgativ(operations));
+
+function power(a,b) {
+	return a ** b;
+}
+
+// Фукция вышего порядка
+function calculate(a,b,fn) {
+	console.log(fn.name);
+	const res = fn(a, b);
+	return res;
+}
+
+let res = calculate(2, 3, add);
+console.log(res);
+res = calculate(2, 3, subtract);
+console.log(res);
+res = calculate(2, 3, power);
+console.log(res);
